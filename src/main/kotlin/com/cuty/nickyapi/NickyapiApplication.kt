ce2b7ab701f00dde1ab350bc927bc6d488156080
Androide <jchocobar886@gmail.com>
2021-01-26 11:30:32 -0300

    Initial commit.

---
+++ src/main/kotlin/com/cuty/nickyapi/NickyapiApplication.kt
@@ -0,0 +1,11 @@
+package com.cuty.nickyapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NickyapiApplication
+
+fun main(args: Array<String>) {
+	runApplication<NickyapiApplication>(*args)
+}
---
+++ src/test/kotlin/com/cuty/nickyapi/NickyapiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.cuty.nickyapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NickyapiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
