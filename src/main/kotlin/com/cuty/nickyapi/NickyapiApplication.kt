f92f7405f4354b93cc489e4e6b32f81d8e5e2c64
Androide <jchocobar886@gmail.com>
2021-02-04 22:52:57 -0300

    subiendo nuevos objetos capsulajr.com

--- src/main/kotlin/com/cuty/nickyapi/NickyapiApplication.kt
+++ src/main/kotlin/com/cuty/nickyapi/NickyapiApplication.kt
@@ -18,7 +18,7 @@ class NickyapiApplication :CommandLineRunner{
 	override fun run(vararg args: String?) {
 		val sandwichDeMila = ItemCarta(nombreItemCarta = "Mila de Carne",precio = 250, ingredientes = "Tomate/Lechuga/Jamon",id = 0, imagen = "https://image.shutterstock.com/image-photo/two-fresh-submarine-sandwiches-ham-260nw-497930494.jpg")
 		itemsRepository!!.save(sandwichDeMila)
-		val itemCarta = ItemCarta(imagen = "https://cdn.imageupload.workers.dev/mTd07heS_hamburguesa.jpg", nombreItemCarta = "Hamburguesa",precio = 310, ingredientes = "jamon/queso/lechuga/tomate", id = 0)
+		val itemCarta = ItemCarta(imagen = "https://capsulajr.com/wp-content/uploads/2021/02/hamburguesa.jpg", nombreItemCarta = "Hamburguesa",precio = 310, ingredientes = "jamon/queso/lechuga/tomate", id = 0)
 		itemsRepository!!.save(itemCarta)
 
 
